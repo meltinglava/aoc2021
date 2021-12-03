a165d4da42f7c9782d8748ebf93b88d6ead2e7f2
meltinglava <roi1996@gmail.com>
2021-12-03 15:43:22 +0100

    improvements day3

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -2,7 +2,7 @@ use std::{
     cmp::Ordering,
     fs::File,
     io::{BufRead, BufReader, Result},
-    ops::{BitXor, Shl, Shr},
+    ops::Shl,
     path::Path,
 };
 
@@ -14,8 +14,7 @@ fn read_lines_as_numbers<P: AsRef<Path>>(path: P) -> Result<Vec<usize>> {
 }
 
 fn digit(n: usize, p: usize) -> usize {
-    assert!((0..12).contains(&p));
-    n.shl((usize::BITS as usize) - 12 + p).shr(usize::BITS - 1)
+    (n & (1 << (11 - p))) >> (11 - p)
 }
 
 fn gamma(input: &[usize]) -> usize {
@@ -35,17 +34,29 @@ fn gamma(input: &[usize]) -> usize {
 
 fn epsilon(n: usize) -> usize {
     let op: usize = 0b_1111_1111_1111;
-    n.bitxor(op)
+    n ^ op
 }
 
-fn life_support_rating(input: &[usize], a: usize) -> usize {
+#[derive(Debug, Clone, Copy)]
+#[repr(usize)]
+enum LSR {
+    CO2,
+    O,
+}
+
+fn life_support_rating(input: &[usize], a: LSR) -> usize {
     let mut input = input.to_vec();
     for p in 0..12 {
-        let find = match (input.iter().cloned().filter(|n| digit(*n, p) == a).count() * 2)
-            .cmp(&input.len())
+        let find = match (input
+            .iter()
+            .cloned()
+            .filter(|n| digit(*n, p) == (a as usize))
+            .count()
+            * 2)
+        .cmp(&input.len())
         {
             Ordering::Less => 0,
-            Ordering::Equal => a,
+            Ordering::Equal => a as usize,
             Ordering::Greater => 1,
         };
         input = input.into_iter().filter(|n| digit(*n, p) == find).collect();
@@ -63,8 +74,8 @@ fn main() -> Result<()> {
     let gam = gamma(&input);
     let eps = epsilon(gam);
     println!("Part1: {}", gam * eps);
-    let oxy = life_support_rating(&input, 1);
-    let c02 = life_support_rating(&input, 0);
+    let oxy = life_support_rating(&input, LSR::O);
+    let c02 = life_support_rating(&input, LSR::CO2);
     println!("Part1: {}", oxy * c02);
     Ok(())
 }
@@ -80,8 +91,7 @@ mod tests {
         for i in 0..12 {
             assert_eq!(digit(d, i), expected, "\nround: {}", i);
             match expected {
-                0 => expected = 1,
-                1 => expected = 0,
+                0 | 1 => expected ^= 1,
                 n => unreachable!("should alway be 0 or 1, found: {}", n),
             }
         }
